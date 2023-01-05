43a3af304cfaf9f2937bc0de19130826551cd194
Chavva <narayanachavva@gail.com>
2023-01-05 09:39:19 +0530

    api endpoints

--- netflix_clone/src/components/Charts.js
+++ netflix_clone/src/components/Charts.js
@@ -1,35 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "../axios.js";
+import Youtube from "react-youtube";
+import '../styles/Charts.css'
+
+
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+
 
 export default function Charts({ title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
 
-
-
-
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
 
     const handleClick = async (movie) => {
-
+        if (trailerUrl) {
+            setTrailerUrl("");
+        } else {
+            let trailerurl = await axios.get(
+                `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+            );
+            setTrailerUrl(trailerurl.data.results[0]?.key);
+        }
     };
 
     return (
         <>
-            <div>
+            <div className="row">
                 <h2>{title}</h2>
 
-                <div>
+                <div className="row_posters">
                     {movies.map(
                         (movie) =>
-                            <img
-                                className=''
-                                alt={movie.name}
-                                key={movie.id}
-                                onClick={() => handleClick(movie)}
-                            />
+                            movie.backdrop_path !== null && (
+                                <img
+                                    className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                                    src={`${baseImgUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path
+                                        }`}
+                                    alt={movie.name}
+                                    key={movie.id}
+                                    onClick={() => handleClick(movie)}
+                                />
+                            )
                     )}
                 </div>
-
+                {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
             </div>
         </>
     )
--- netflix_clone/src/components/Header.js
+++ netflix_clone/src/components/Header.js
@@ -1,13 +1,24 @@
 import React, { useEffect, useState } from 'react'
+import axios from "../axios";
+import requests from "../requests";
 import '../styles/Header.css'
 
 
 export default function Header() {
 
     const [movie, setMovie] = useState();
-
-
-
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        fetchData();
+    }, []);
 
 
     function truncate(str, n) {
@@ -16,19 +27,27 @@ export default function Header() {
 
     return (
         <>
-            <header className='banner'>
-
+            <header
+                className="banner"
+                style={{
+                    backgroundSize: "cover",
+                    backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+                    backdropPosition: "center center",
+                }}
+            >
                 <div className="banner_contents">
-                    <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
+                    <h1 className="banner_title">
+                        {movie?.title || movie?.name || movie?.original_name}
+                    </h1>
 
                     <div className="banner_buttons">
                         <button className="banner_button">Play</button>
-                        <button className="banner_button">My List</button>
+                        <button className="banner_button">My List </button>
                     </div>
 
-                    <h1>{truncate(movie?.overview, 200)}</h1>
+                    <h1 className="banner_description">{truncate(movie?.overview, 200)}</h1>
                 </div>
-
+                <div className="banner_fadeBottom" />
             </header>
 
         </>
